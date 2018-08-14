e7976455e388ef6efca1ca413360a196279087ed
tnino <30389051+tnino@users.noreply.github.com>
2018-08-14 10:26:53 -0500

    Create init.js

---
+++ init.js
@@ -0,0 +1,20 @@
+<script> 
+$(document).ready(function(){
+    $("#btn_firstName").click(function(){
+        $("#firstName").slideDown("slow");
+    });
+
+    $("#btn_lastName").click(function(){
+        $("#lastName").slideDown("slow");
+    });
+    $("#btn_homeOffice").click(function(){
+        $("#homeOffice").slideDown("slow");
+    });
+    $("#btn_position").click(function(){
+        $("#position").slideDown("slow");
+    });
+    $("#btn_tellMeMore").click(function(){
+        $("#tellMeMore").slideDown("slow");
+    });
+});
+</script>
